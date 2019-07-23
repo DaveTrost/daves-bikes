900a113abc738c10c40534b03d54f9e52de7aa51
Dave Trost <davetrost@gmail.com>
2019-07-23 10:37:00 -0700

    generate html from bikes list

--- src/generate-bikes-list.js
+++ src/generate-bikes-list.js
@@ -1,10 +1,10 @@
-import bikes from 'data/bikes.js';
-import generateBike from 'generate-bike.js';
+import { bikes } from './data/bikes.js';
+import { generateBike } from './generate-bike.js';
 
-//const list = document.getElementById('bikes-list');
-//
-//for(let i = 0; i < bikes.length; i++) {
-//    const bike = bikes[i];
-//    const dom = generateBike(bike);
-//    list.appendChild(dom);
-//}
+const bikesList = document.getElementById('bikes');
+
+for(let i = 0; i < bikes.length; i++) {
+    const bike = bikes[i];
+    const dom = generateBike(bike);
+    bikesList.appendChild(dom);
+}
