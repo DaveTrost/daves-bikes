1d83d660741bc583c712e19d43b8f6ef4e767883
Dave Trost <davetrost@gmail.com>
2019-07-23 09:23:02 -0700

    created bikes data

--- src/data/bikes.js
+++ src/data/bikes.js
@@ -1,13 +1,93 @@
-
 const bmx20 = {
     code: 'bmx-20',
-    name: '20" BMX bike',
+    name: '20" BMX Bike',
     image: 'assets/bmx-20.jpg',
     description: 'A trick machine ready for back flips at the bike park',
     category: 'bmx',
     price: 500.00,
     cost: 200
 };
+
+const bmxDJ = {
+    code: 'bmx-dj',
+    name: '26" Dirt Jumper',
+    image: 'assets/bmx-dj.jpg',
+    description: 'Get the biggest airs of your life',
+    category: 'bmx',
+    price: 700.00,
+    cost: 400
+};
+
+const bmxKids = {
+    code: 'bmx-kids',
+    name: '16" Kids BMX Bike',
+    image: 'assets/bmx-kids.jpg',
+    description: 'for the lil\' shredders working on their sick skills',
+    category: 'bmx',
+    price: 300.00,
+    cost: 150
+};
+
+const hybridCyclocross = {
+    code: 'hybrid-cyclocross',
+    name: 'Cyclocross Bike',
+    image: 'assets/hybrid-cyclocross.jpg',
+    description: 'Fast enough for pavement, nimble enough for the singletrack and burly through the mud',
+    category: 'hybrid',
+    price: 1000.00,
+    cost: 600
+};
+
+const hybridFatTire = {
+    code: 'hybrid-fat-tire',
+    name: 'Fat Tire Bike',
+    image: 'assets/hybrid-fat-tire.jpg',
+    description: 'The monster truck of bicycles. Bring it on.',
+    category: 'hybrid',
+    price: 1500.00,
+    cost: 700
+};
+
+const hybridGravel = {
+    code: 'hybrid-gravel',
+    name: 'Gravel Bike',
+    image: 'assets/hybrid-gravel.jpg',
+    description: 'smooth out the roughest roads and carve the turns',
+    category: 'hybrid',
+    price: 900.00,
+    cost: 400
+};
+
+const mountainDownhill = {
+    code: 'mountain-downhill',
+    name: 'Downhill Bike',
+    image: 'assets/mountain-downhill.jpg',
+    description: 'Big drop? No problem. Crush the jumps on this big travel bike',
+    category: 'mountain',
+    price: 2400.00,
+    cost: 1600
+};
+
+const mountainDual = {
+    code: 'mountain-dual',
+    name: 'Dual Suspension Mountain Bike',
+    image: 'assets/mountain-dual.jpg',
+    description: 'Light and stiff carbon fiber frame and the most reactive suspension ever',
+    category: 'mountain',
+    price: 1900.00,
+    cost: 1200
+};
+
+const mountainHardtail = {
+    code: 'mountain-hardtail',
+    name: 'Hardtail Mountain Bike',
+    image: 'assets/mountain-hardtail.jpg',
+    description: 'Ride the trails in style with this new-old-school bike',
+    category: 'mountain',
+    price: 1300.00,
+    cost: 900
+};
+
 
 export const bikes = [
     bmx20
